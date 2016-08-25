ba2c969d2a202a6c21616ab5a72bf55e835b3fd0
hfoffani <hfoffani@gmail.com>
2016-08-25 17:31:40 +0200

    update to ipy 2.7.6.3

--- issues/test957.py
+++ issues/test957.py
@@ -1,5 +1,7 @@
 import os
-basedir = 'C:\\Program Files (x86)\\IronPython 2.7\\' 
+import clr
+
+basedir = 'C:\\Program Files (x86)\\IronPython 2.7\\'
 all_files = []
 for dir, blah, files in os.walk(basedir + 'Lib'):
     if dir.startswith('Lib\\site-packages'):
@@ -7,7 +9,8 @@ for dir, blah, files in os.walk(basedir + 'Lib'):
         continue
     for file in files:
         if file.endswith('.py'):
+            if file in [ 'platform.py'] :
+                continue
             all_files.append(dir + '\\' + file)
 
-import clr
 clr.CompileModules('z:\\Documents\\ipy-issues\\issues\\LibPy.dll', *all_files)
